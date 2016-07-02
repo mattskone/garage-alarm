6c7e07079b1683e33765846e784bad7a8e5c0da8
Matt Skone <matt.skone@gmail.com>
2016-07-02 03:47:45 +0000

    Corrected cron script format

--- setcron.sh
+++ setcron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-line="0,30 6-20 * * * python /home/pi/src/garage-alarm/run.py"
+line="*/30 0-4,15-23 * * * python /home/pi/src/garage-alarm/run.py"
 (crontab -l; echo "$line") | crontab -
 
